4e107eb91d48c7cedde22a0c09a4741816e399b6
DASADeveloper <tallerwebinformatica@gmail.com>
2020-05-29 16:42:56 -0600

    RestServer

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+ 
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,42 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+//Configuracion global de las rutas
+app.use ( require('./routes/index') );
 
-app.get('/usuario', function (req, res) {
-    res.json('Get Usuario')
+
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, res)=>{
+
+    if(err) throw err;
+
+    console.log('Base de datos ONLINE');
+
 });
 
-
-app.post('/usuario', function (req, res) {
-    
-    let body = req.body;
-
-    if( body.nombre === undefined ){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    }else{
-        res.json({
-            persona: body
-        });
-    }
-    
-});
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('Delete Usuario')
-});
-
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 app.listen(process.env.PORT, () => {
     console.log('Running ', process.env.PORT);
