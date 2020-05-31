ed90a4639f0c2ebeaa59b283e8ad1b63373665ef
DASADeveloper <tallerwebinformatica@gmail.com>
2020-05-31 10:30:30 -0600

    Login Normal

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,9 +3,12 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const { verificarToken, verificarAdmin_Role } = require('../middlewares/autenticacion');
+
 const app = express();
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificarToken , (req, res) => {
+
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -39,7 +42,7 @@ app.get('/usuario', function (req, res) {
 });
 
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificarToken, verificarAdmin_Role], (req, res) => {
     
     let body = req.body;
 
@@ -69,7 +72,7 @@ app.post('/usuario', function (req, res) {
 
 });
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificarToken, verificarAdmin_Role] , (req, res) => {
     
     let id = req.params.id;
     let body =  _.pick( req.body, ['nombre','email','img','role','estado']);
@@ -95,7 +98,7 @@ app.put('/usuario/:id', function (req, res) {
 
 });
 
-app.delete('/usuario', function (req, res) {
+app.delete('/usuario/:id', [verificarToken, verificarAdmin_Role] , (req, res) => {
     let id = req.params.id;
 
     // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
